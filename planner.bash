60f600233688b8985eb7749d8cdb8b5b0d587da2
IlMusu <mattia.musumeci@yahoo.it>
2022-05-04 20:03:38 +0200

    result problem 4

--- planner.bash
+++ planner.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
-java -Xmx28G -jar ./ENHSP/enhsp.jar -o $1 -f $2 -planner opt-blind -pe
+java -jar ./ENHSP/enhsp.jar -o $1 -f $2 -anytime -pe
 #java -jar ./ENHSP/enhsp.jar -o $1 -f $2 -anytime -pe
